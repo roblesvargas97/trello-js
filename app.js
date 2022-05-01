c7b3b29f7efa41427ba1ecf40489ccd5c25b44bd
roblesvargas97 <roblesvargas97@gmail.com>
2022-05-01 15:25:48 -0500

    Añadiendo metodo para trello

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 require('dotenv').config()
 
 if(!process.env.TOKEN && !process.env.KEY){
-  throw new Error('No hay configuración con Api Key y con Token')
+	throw new Error('No hay configuración con Api Key y con Token')
 }
+
+const Trello = require("trello");
+var trello = new Trello(process.env.KEY, process.env.TOKEN);
+var cardTitle = `Card Nueva ${new Date()}`;
+
+console.log("Ejecutando!");
+
+trello.addCard(cardTitle, "LaunchX Card Description", "626d6dfde90f063a2af74713",
+  function (error, trelloCard) {
+    if (error) {
+      console.log('Could not add card:', error);
+    }
+    else {
+      console.log('Added card:', trelloCard);
+    }
+  });
